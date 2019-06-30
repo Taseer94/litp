77c135a3341488f5a186857a7079dc79110c248c
Taseer <taseer94@gmail.com>
2019-06-30 13:41:21 +0200

    fix error

--- bootstrap.sh
+++ bootstrap.sh
@@ -31,6 +31,7 @@ install_kubectl () {
   curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
   chmod +x ./kubectl
   sudo mv ./kubectl /usr/local/bin/kubectl
+}
 
 ###########################
 #  Install AWS CLI
