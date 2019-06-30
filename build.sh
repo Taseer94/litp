f29a20e42bdd16a8e6cbe2740d06d113dcd3f783
Taseer <taseer94@gmail.com>
2019-06-30 14:10:09 +0200

    use the pull request env var

--- build.sh
+++ build.sh
@@ -6,23 +6,25 @@
 #   -o pipefail: Fail on errors in scripts this calls, give stacktrace
 set -ex -o pipefail
 
+echo ${TRAVIS_PULL_REQUEST_BRANCH}
+
 ###########################
 # Terraform deployment
 ###########################
 deploy_terraform () {
   echo "  Deploying Terraform "
-  if [[ ${TRAVIS_BRANCH} =~ 'azure' ]]; then
+  if [[ ${TRAVIS_PULL_REQUEST_BRANCH} =~ 'azure' ]]; then
     cd infrastructure/azure/envs/stage
     terraform init -no-color
     terraform validate -no-color
     terraform plan -no-color
     terraform apply -no-color -auto-approve
-  elif [[ ${TRAVIS_BRANCH} =~ 'gpc' ]]; then
+  elif [[ ${TRAVIS_PULL_REQUEST_BRANCH} =~ 'gpc' ]]; then
     cd infrastructure/gcp/envs/stage
     terraform validate
     terraform plan -no-color
     terraform apply -no-color -auto-approve
-  elif [[ ${TRAVIS_BRANCH} =~ 'aws' ]]; then
+  elif [[ ${TRAVIS_PULL_REQUEST_BRANCH} =~ 'aws' ]]; then
     cd infrastructure/aws/envs/stage
     terraform validate
     terraform plan -no-color
@@ -35,10 +37,10 @@ deploy_terraform () {
 ###########################
 test_k8s () {
   echo "Testing the deployment"
-  if [[ ${TRAVIS_BRANCH} =~ 'azure' ]]; then
+  if [[ ${TRAVIS_PULL_REQUEST_BRANCH} =~ 'azure' ]]; then
     echo "Getting the Azure Kubeconfig"
     az aks get-credentials --resource-group kaos-2-stage-k8s --name kaos-2-stage-k8s
-  elif [[ ${TRAVIS_BRANCH} =~ 'aws' ]]; then
+  elif [[ ${TRAVIS_PULL_REQUEST_BRANCH} =~ 'aws' ]]; then
     echo "Getting the AWS Kubeconfig"
     aws eks update-kubeconfig --name kaos-2-stage-eks-cluster --region eu-west-3
   fi
